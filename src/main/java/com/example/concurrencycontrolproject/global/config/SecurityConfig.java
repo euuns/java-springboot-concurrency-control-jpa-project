95a9ee9740f8906d1298fd3f99ea6cbaebc57029
euuns <dmsu2@naver.com>
2025-03-28 20:44:31 +0900

    Feat: OAuth2.0 적용을 위한 config 파일 작성
    - SecurityConfig: oauth2Login추가
    - OAuth2LoginConfig: 구글 관련 설정 구현 (추가 필요)

--- src/main/java/com/example/concurrencycontrolproject/global/config/SecurityConfig.java
+++ src/main/java/com/example/concurrencycontrolproject/global/config/SecurityConfig.java
@@ -8,10 +8,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.example.concurrencycontrolproject.authentication.oauth2.handler.OAuth2AuthenticationFailureHandler;
+import com.example.concurrencycontrolproject.authentication.oauth2.handler.OAuth2AuthenticationSuccessHandler;
 import com.example.concurrencycontrolproject.global.filter.ExceptionHandlerFilter;
 import com.example.concurrencycontrolproject.global.filter.JwtAuthenticationFilter;
 
@@ -25,6 +30,8 @@ public class SecurityConfig {
 
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 	private final ExceptionHandlerFilter exceptionHandlerFilter;
+	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+	private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -32,22 +39,34 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http,
+		OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2Service) throws Exception {
 		return http
 			.csrf(AbstractHttpConfigurer::disable)
+
 			.sessionManagement((session) -> session
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
 			.addFilterBefore(jwtAuthenticationFilter, SecurityContextHolderAwareRequestFilter.class)
 			.addFilterBefore(exceptionHandlerFilter, JwtAuthenticationFilter.class)
+
 			.formLogin(AbstractHttpConfigurer::disable)
 			.anonymous(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.logout(AbstractHttpConfigurer::disable)
 			.rememberMe(AbstractHttpConfigurer::disable)
+
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(new AntPathRequestMatcher("/api/*/auth/**")).permitAll()
 				.anyRequest().authenticated())
+
+			.oauth2Login(oauth2 -> oauth2
+				.userInfoEndpoint(userInfo -> userInfo
+					.userService(oauth2Service))
+				.successHandler(oAuth2AuthenticationSuccessHandler)
+				.failureHandler(oAuth2AuthenticationFailureHandler)
+			)
+
 			.build();
 	}
-
 }
