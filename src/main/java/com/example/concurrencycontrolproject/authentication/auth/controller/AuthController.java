b6ca9ee5769919e57ef764547b4c4fd308d43c0b
euuns <dmsu2@naver.com>
2025-03-28 20:41:31 +0900

    Feat: 소셜 로그인 유저에게 추가 정보를 받는 controller, service 작성

--- src/main/java/com/example/concurrencycontrolproject/authentication/auth/controller/AuthController.java
+++ src/main/java/com/example/concurrencycontrolproject/authentication/auth/controller/AuthController.java
@@ -1,14 +1,18 @@
-package com.example.concurrencycontrolproject.auth.controller;
+package com.example.concurrencycontrolproject.authentication.auth.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.concurrencycontrolproject.auth.dto.SigninRequest;
-import com.example.concurrencycontrolproject.auth.dto.SignupRequest;
-import com.example.concurrencycontrolproject.auth.dto.SignupResponse;
-import com.example.concurrencycontrolproject.auth.service.AuthService;
+import com.example.concurrencycontrolproject.authentication.auth.dto.AdditionalInfoRequest;
+import com.example.concurrencycontrolproject.authentication.auth.dto.SigninRequest;
+import com.example.concurrencycontrolproject.authentication.auth.dto.SignupRequest;
+import com.example.concurrencycontrolproject.authentication.auth.dto.SignupResponse;
+import com.example.concurrencycontrolproject.authentication.auth.dto.UserProfileResponse;
+import com.example.concurrencycontrolproject.authentication.auth.service.AuthService;
+import com.example.concurrencycontrolproject.authentication.oauth2.dto.OAuthUser;
 import com.example.concurrencycontrolproject.domain.common.response.Response;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -34,5 +38,13 @@ public class AuthController {
 		HttpServletResponse servletResponse) {
 		authService.signin(signinRequest.getEmail(), signinRequest.getPassword(), servletResponse);
 		return Response.empty();
+	}
+
+	// 첫 소셜 로그인 유저 -> 추가 정보 입력
+	@PostMapping("/v1/auth/privacy-consent")
+	public Response<UserProfileResponse> agreeToPrivacyPolicy(@AuthenticationPrincipal OAuthUser oAuthUser,
+		@Valid @RequestBody AdditionalInfoRequest addInfo) {
+		UserProfileResponse userProfile = authService.addUserInfo(oAuthUser, addInfo);
+		return Response.of(userProfile);
 	}
 }
--- src/main/java/com/example/concurrencycontrolproject/authentication/auth/service/AuthService.java
+++ src/main/java/com/example/concurrencycontrolproject/authentication/auth/service/AuthService.java
@@ -1,14 +1,22 @@
-package com.example.concurrencycontrolproject.auth.service;
+package com.example.concurrencycontrolproject.authentication.auth.service;
 
+import java.util.EnumSet;
 import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.example.concurrencycontrolproject.auth.dto.SignupResponse;
-import com.example.concurrencycontrolproject.auth.jwt.JwtUtil;
+import com.example.concurrencycontrolproject.authentication.auth.dto.AdditionalInfoRequest;
+import com.example.concurrencycontrolproject.authentication.auth.dto.SignupResponse;
+import com.example.concurrencycontrolproject.authentication.auth.dto.UserProfileResponse;
+import com.example.concurrencycontrolproject.authentication.exception.DuplicateSocialEmailException;
+import com.example.concurrencycontrolproject.authentication.exception.UnsupportedProviderException;
+import com.example.concurrencycontrolproject.authentication.jwt.service.RefreshTokenService;
+import com.example.concurrencycontrolproject.authentication.jwt.util.JwtUtil;
+import com.example.concurrencycontrolproject.authentication.oauth2.dto.OAuthUser;
 import com.example.concurrencycontrolproject.domain.user.entity.User;
+import com.example.concurrencycontrolproject.domain.user.enums.SocialType;
 import com.example.concurrencycontrolproject.domain.user.exception.AlreadyExistsEmailException;
 import com.example.concurrencycontrolproject.domain.user.exception.EmailAccessDeniedException;
 import com.example.concurrencycontrolproject.domain.user.exception.EmailNotFoundException;
@@ -33,8 +41,8 @@ public class AuthService {
 		}
 
 		String encodedPassword = bCryptPasswordEncoder.encode(password);
-		User User = new User(email, encodedPassword, nickname, phoneNumber);
-		User saveUser = userRepository.save(User);
+		User user = new User(email, encodedPassword, nickname, phoneNumber);
+		User saveUser = userRepository.save(user);
 
 		return SignupResponse.from(saveUser);
 	}
@@ -54,6 +62,24 @@ public class AuthService {
 		createAndSaveJwt(user, servletResponse);
 	}
 
+	public UserProfileResponse addUserInfo(OAuthUser oAuthUser, AdditionalInfoRequest request) {
+		String email = oAuthUser.getEmail();
+		String nickname = oAuthUser.getNickname();
+		SocialType social = getSocialType(oAuthUser.getProviderType());
+		String password = request.getPassword();
+		String phoneNumber = request.getPhoneNumber();
+
+		if (userRepository.existsByEmailAndSocial(email, social)) {
+			throw new DuplicateSocialEmailException();
+		}
+
+		String encodedPassword = bCryptPasswordEncoder.encode(password);
+		User user = new User(email, encodedPassword, nickname, phoneNumber, social);
+		User saveUser = userRepository.save(user);
+
+		return UserProfileResponse.from(saveUser);
+	}
+
 	private void createAndSaveJwt(User user, HttpServletResponse servletResponse) {
 		String accessToken = jwtUtil.createAccessToken(user.getId(), user.getEmail(), user.getRole(),
 			user.getNickname());
@@ -62,5 +88,12 @@ public class AuthService {
 		String refreshToken = jwtUtil.createRefreshToken(user.getId());
 		jwtUtil.refreshTokenSetCookie(refreshToken, servletResponse);
 		redisCache.saveRefreshToken(refreshToken, String.valueOf(user.getId()));
+	}
+
+	private SocialType getSocialType(String social) {
+		return EnumSet.allOf(SocialType.class).stream()
+			.filter(e -> e.name().equalsIgnoreCase(social))
+			.findFirst()
+			.orElseThrow(UnsupportedProviderException::new);
 	}
 }
